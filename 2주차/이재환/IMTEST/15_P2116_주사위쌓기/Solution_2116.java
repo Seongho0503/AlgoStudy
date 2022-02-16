391d674cedfa7271a1c15f7eaaec104423999591
baboeyo <97875998+baboeyo@users.noreply.github.com>
2022-02-16 16:16:25 +0900

    Update Solution_2116.java

--- 2주차/이재환/IMTEST/15_P2116_주사위쌓기/Solution_2116.java
+++ 2주차/이재환/IMTEST/15_P2116_주사위쌓기/Solution_2116.java
@@ -45,20 +45,21 @@ public class Solution_2116 {
 		System.out.println(max);
 	}
 	
-	// 밑면(bottom) 구하는 함수
+	// 밑면(bottom) 구하는 method
 	public static int bottom(int[] arr,int i) {
 		int bottom=0;
-		if(i==0) 			bottom=arr[i+5];
+		if(i==0) 	    bottom=arr[i+5];
 		else if(i==1||i==2) bottom=arr[i+2];
 		else if(i==3||i==4) bottom=arr[i-2];
-		else if(i==5) 		bottom=arr[i-5]; 
+		else if(i==5) 	    bottom=arr[i-5]; 
 		return bottom;
 	}
 	
+	//옆면 최댓값 구하는 method
 	public static int sideSum(int bottom,int top,int sum) {
-		if(bottom+top==11) 			sum+=4; //두 면 6,5 -> 옆면 최댓값 4
-		else if(bottom==6||top==6)  sum+=5; //한 면 6 -> 옆면 최댓값 5
-		else						sum+=6;	//6이 옆면에 있을때 -> 옆면 최댓값 6
+		if(bottom+top==11)	   sum+=4; //두 면 6,5 -> 옆면 최댓값 4
+		else if(bottom==6||top==6) sum+=5; //한 면 6 -> 옆면 최댓값 5
+		else	                   sum+=6;	//6이 옆면에 있을때 -> 옆면 최댓값 6
 		return sum;
 	}
 }
