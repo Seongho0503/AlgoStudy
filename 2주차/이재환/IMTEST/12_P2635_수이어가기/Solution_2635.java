c8f348347cec61f29ef5cff6e12461cda8d4766a
baboeyo <97875998+baboeyo@users.noreply.github.com>
2022-02-21 23:31:49 +0900

    Update Solution_2635.java
    Update IM12

--- 2주차/이재환/IMTEST/12_P2635_수이어가기/Solution_2635.java
+++ 2주차/이재환/IMTEST/12_P2635_수이어가기/Solution_2635.java
@@ -15,22 +15,33 @@ public class Solution_2635 {
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		//첫번째 수 입력
 		int First= Integer.parseInt(br.readLine());
 		
 		queue = new LinkedList<>();
 		
-		cnt=2;
+		cnt=2; //최대 갯수(초기값)
 		int idx=0;
 		int max=0;
+		
+		//(가능한 최대 갯수, 두번째 수 구하기)
 		for(int i=First/2;i<=First;i++) {
 			if(cal(2,First,i)>max) {
 				max=cal(2,First,i);
 				idx=i;
 			}
 		}
+		//큐 초기화
 		queue.clear();
+		
+		//첫 번째 수 입력
 		queue.offer(First);
+		
+		//두번째 수 입력
 		queue.offer(idx);
+		
+		//세번째~MAX번째 수 계산
 		cal(2,First,idx);
 		
 		//출력
@@ -40,6 +51,7 @@ public class Solution_2635 {
 
 	}
 	
+	//세번째 값 만들기 (가능하면 재귀, 불가능하면 최대 갯수 return)
 	public static int cal(int cnt,int first, int second){
 		int third = first-second;
 		if(third<0)
